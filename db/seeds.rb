9de10bf852c5947b76b93d4142dfc98b3277f5fa
Stéfany Larissa <stelar.dev@gmail.com>
2022-06-19 23:21:40 -0300

    faker added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Survivor.create name: Faker::Name.name, age: Faker::Number.number(digits: 2), gender: Faker::Gender.type, last_latitude: Faker::Address.latitude, last_longitude: Faker::Address.longitude
+end
