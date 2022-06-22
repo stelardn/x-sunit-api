fae9ab092ede278a619694c95f0a71124ec82f1b
Stéfany Larissa <stelar.dev@gmail.com>
2022-06-21 23:26:11 -0300

    abductions ok

--- app/controllers/api/abductions_controller.rb
+++ app/controllers/api/abductions_controller.rb
@@ -1,6 +1,5 @@
 class Api::AbductionsController < ApplicationController
-  before_action :set_abduction, only: %i[ show update destroy ]
-  
+  before_action :set_abduction, only: %i[ show ]
   
   # GET /abductions
   def index
@@ -17,58 +16,5 @@ class Api::AbductionsController < ApplicationController
 
     render json: "Current abduction percentage: #{abduction_rate}%. \nNon-abduction percentage: #{non_abduction_rate}%."
   end
-
-  # # GET /survivors/1
-  # def show
-  #   render json: @survivor
-  # end
-
-  # # POST /survivors
-  # def create
-  #   @survivor = Survivor.new(survivor_params)
-
-  #   if @survivor.save
-  #     render json: @survivor, status: :created, location: @survivor
-  #   else
-  #     render json: @survivor.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # PATCH/PUT /survivors/1
-  # def update
-  #   if @survivor.update(survivor_update_params)
-  #     render json: @survivor
-  #   else
-  #     render json: @survivor.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # DELETE /survivors/1
-  # def destroy
-  #   @survivor.destroy
-  # end
-
-  # # generate report
-  # def report
-  #   render json: @report
-  # end
-
-
-
-
-  # private
-  #   # Use callbacks to share common setup or constraints between actions.
-  #   def set_abduction
-  #     @abduction = Survivor.find(params[:id])
-  #   end
-
-  #   # Only allow a list of trusted parameters through.
-  #   def survivor_params
-  #     params.require(:survivor).permit(:id, :name, :age, :gender, :last_latitude, :last_longitude, :created_at, :updated_at, :flags, :abduction)
-  #   end
-
-  #   def survivor_update_params
-  #     params.require(:survivor).permit(:age, :gender, :last_latitude, :last_longitude)
-  #   end
 
 end
